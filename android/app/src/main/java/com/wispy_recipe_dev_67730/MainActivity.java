db32cc5a82e868aa719a8f89cc81814c2a7fc35d
Crowdbotics <engineering@crowdbotics.com>
2023-03-03 06:17:41 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/wispy_recipe_dev_67730/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wispy_recipe_dev_67730;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "wispy_recipe_dev_67730";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wispy-recipe-dev-67730",
+  url: "https://wispy-recipe-dev-67730.botics.co",
+  api: "https://wispy-recipe-dev-67730.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
